4d0526159bf394f0304260018d18e0445290fe0d
Kevin Sweet <kevindsweet@gmail.com>
2015-04-07 11:23:39 -0500

    logo work

--- themes/worldly/assets/js/application.js
+++ themes/worldly/assets/js/application.js
@@ -1,3 +1,9 @@
+// on mobile / touch devices, get rid of the fadeOut titles on image posts
+if (Modernizr.touch)
+  skrollr.init().destroy();
+else
+  var s = skrollr.init();
+
 $(function() {
   // dropcaps
   // $('article > p:first-child').html(function (i, html) {
