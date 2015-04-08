103ed5193d6a8edf32fb7c9dcaf330299f63f454
Kevin Sweet <kevindsweet@gmail.com>
2015-04-08 14:18:09 -0500

    mobile edits

--- themes/worldly/assets/js/components/hero.js
+++ themes/worldly/assets/js/components/hero.js
@@ -1,6 +1,10 @@
 $(function() {
   // adjust hero height, in case vh units don't work
-  $('.hero, .hero--about').height($(window).height());
+  function resizeHero() {
+    $('.hero, .hero--about, .hero--post').height($(window).height());
+  }
+  resizeHero();
+  $(window).on('resize', resizeHero);
 
   // scroll to content when hero down arrows are clicked
   $('.hero__read-more-arrows').on('click', function(e) {
