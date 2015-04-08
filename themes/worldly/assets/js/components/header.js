3fbb85b12dedb1072cd1713d28a1f0608de1b005
Kevin Sweet <kevindsweet@gmail.com>
2015-04-08 13:35:24 -0500

    mobile header

--- themes/worldly/assets/js/components/header.js
+++ themes/worldly/assets/js/components/header.js
@@ -1,5 +1,17 @@
 $(function() {
   $('.header__hamburger').on('click', function() {
-    $('.header__nav-links').toggle();
+    $('body').addClass('header__menu-open');
+  });
+
+  $('.header__nav-close').on('click', function() {
+    $('body').removeClass('header__menu-open');
+  });
+
+  $(document).mouseup(function (e) {
+    var container = $('.header__nav-links');
+
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      $('body').removeClass('header__menu-open');
+    }
   });
 });
--- themes/worldly/assets/js/components/hero.js
+++ themes/worldly/assets/js/components/hero.js
@@ -1,6 +1,6 @@
 $(function() {
   // adjust hero height, in case vh units don't work
-  $('.hero, .hero--post').height($(window).height());
+  $('.hero, .hero--about').height($(window).height());
 
   // scroll to content when hero down arrows are clicked
   $('.hero__read-more-arrows').on('click', function(e) {
