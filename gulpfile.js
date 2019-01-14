69870995af98d718e26883da9f316a4de3567abb
Kevin Sweet <kevindsweet@gmail.com>
2019-01-13 20:12:09 -0700

    added gulp-gm

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,7 @@ var gulp         = require('gulp'),
     rev          = require('gulp-rev'),
     revReplace   = require('gulp-rev-replace'),
     fingerprint  = require('gulp-fingerprint'),
+    gm           = require('gulp-gm'),
     reload       = browserSync.reload;
 
 // scripts
